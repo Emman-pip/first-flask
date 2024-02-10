fa263242a39c258eb612979f75848c963b65f57f
emman-pip <parrenoemmanuel756@gmail.com>
2024-02-10 16:30:16 +0800

    Set up for linux development.

--- board/__init__.py
+++ board/__init__.py
@@ -13,6 +13,6 @@ def create_app():
     app.register_blueprint(pages.bp)
     app.register_blueprint(posts.bp)
     
-    print(f"Current Environment: {os.getenv("ENVIRONMENT")}")
-    print(f"Using Database: {app.config.get("DATABASE")}")
+    print(f"Current environment: {os.getenv('ENVIRONMENT')}")
+    print(f"Using Database: {app.config.get('DATABASE')}")
     return app
