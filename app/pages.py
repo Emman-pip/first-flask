bbd160c953b2ee3101b980d924dae504f1450617
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2024-02-18 09:34:30 +0800

    Add missions page (unfinished).

--- app/pages.py
+++ app/pages.py
@@ -6,3 +6,8 @@ bp = Blueprint('pages', __name__)
 @bp.route('/')
 def home():
     return render_template('pages/home.html')
+
+@bp.route('/missions')
+def mission():
+    data = app.missions.query.all()
+    return render_template('pages/missions.html', missions=data)
