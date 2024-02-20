fc31541464a4f43bb0d994ed5e81183592fbe5b4
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2024-02-20 16:50:17 +0800

    Change initialization of database.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, Integer, String, DateTime, Boolean 
 import os
 from dotenv import load_dotenv
 
@@ -7,17 +9,22 @@ from app import pages
 
 load_dotenv()
 
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASEURI') 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.register_blueprint(pages.bp)
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
 class missions(db.Model):
-    missionID = db.Column(db.Integer, primary_key=True)
-    spacecraft = db.Column(db.String(100), nullable=False)
-    launchDate = db.Column(db.DateTime, nullable=False)
-    operator = db.Column(db.String(100), nullable=False)
-    outcome = db.Column(db.Boolean, nullable=False)
+    missionID = Column(Integer, primary_key=True)
+    spacecraft = Column(String(100), nullable=False)
+    launchDate = Column(DateTime, nullable=False)
+    operator = Column(String(100), nullable=False)
+    outcome = Column(Boolean, nullable=False)
     
