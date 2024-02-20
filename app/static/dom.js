d3ba7ac3397836baf655e731a113021dd1c1214c
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2024-02-21 07:27:24 +0800

    Add animation to burger.

--- app/static/dom.js
+++ app/static/dom.js
@@ -1,11 +1,17 @@
 const navToggle = () => {
   const burger = document.querySelector(".burger");
   const target = document.querySelector(".left");
+  const line1 = document.querySelector(".one");
+  const line2 = document.querySelector(".two");
+  const line3 = document.querySelector(".three");
 
   burger.addEventListener("click", (e) => {
     console.log("clicked")
     // target.style.transform = "translateX(10%)"
     target.classList.toggle("toView")
+    line1.classList.toggle("first")
+    line2.classList.toggle("second")
+    line3.classList.toggle("third")
     // document.querySelector("body").style.backgroundColor = "white";
   });
 };
